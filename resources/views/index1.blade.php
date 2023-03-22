329bd8c047900da39d5ecda3f92ac6db2ac19ee7
Rifqi <rifqi.asyari24@gmail.com>
2023-03-22 14:27:11 +0700

    first commit

---
+++ resources/views/index1.blade.php
@@ -0,0 +1,16 @@
+@extends ('layout.app')
+
+@section('title')
+    Index1
+@endsection
+
+@section('content')
+    <h1>Index1</h1>
+    <p>ini page index1</p>
+    <a href="/about">about</a>
+    <a href="/home">home</a>
+    <a href="/index2">index2</a>
+
+    
+    
+@endsection
---
+++ resources/views/lea/index2.blade.php
@@ -0,0 +1,16 @@
+<@extends ('layout.app')
+
+@section('title')
+    Index2
+@endsection
+
+@section('content')
+    <h1>Index2</h1>
+    <p>ini page index2</p>
+    <a href="/about">about</a>
+    <a href="/home">home</a>
+    <a href="/index1">index1</a>
+
+    
+    
+@endsection
